2c348c7bdbfb5067ff05e024fc22f07c59c595dc
Yara Studer <yarastuder@gmail.com>
2026-03-27 17:18:31 -0400

    Removed quick mode, updated config.js and disruptions.js to be better templates

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const config = {
 
     // Video stimuli filepath
-    VIDEO_PATH: "assets/video/stimuli",
+    VIDEO_PATH: "assets/videos/stimuli",
 
     // When enabled, shuffles order of all videos
     // Note: this overrides ALL other shuffle parameters
@@ -12,60 +12,24 @@ export const config = {
 
     // When enabled, ensures that if a participant sees a video from List A, they won't see the corresponding version at the same index in List B (or any other list)
     // See README for more information
-    EXCLUSIVE_INDEX_MODE: true,
+    EXCLUSIVE_INDEX_MODE: false,
 
     // When enabled, logs debug messages to console
     DEBUG_LOGS: false,
-
-    // When enabled, skips instructions and demographics
-    DEBUG_QUICK: false,
 
     // When enabled, saves data locally
     DEBUG_SAVE: false
 };
 
 export const videoLists = [
-    // {
-    //     condition: "test",
-    //     selectionNum: 1,
-    //     shuffle: false,
-    //     videos: [
-    //         "Placeholder10.mp4",
-    //         "Placeholder15.mp4"
-    //     ]
-    // },
     {
         condition: "control", // The condition name saved in data
-        selectionNum: 5, // How many videos to select from this list
-        shuffle: false, // Whether to shuffle the order of the videos or not
+        selectionNum: 3, // How many videos to select from this list
+        shuffle: false, // Whether to shuffle the order of the videos or not (overriden by SHUFFLE_ALL)
         videos: [
-            "P3.mp4",
-            "P7.mp4",
-            "P8.mp4",
-            "P9.mp4",
-            "P10.mp4",
-            "P12.mp4",
-            "P13.mp4",
-            "P14.mp4",
-            "P16.mp4",
-            "P18.mp4"
+            "video1.mp4",
+            "video2.mp4",
+            "video3.mp4"
         ]
-    },
-    {
-        condition: "early",
-        selectionNum: 5,
-        shuffle: false,
-        videos: [
-            "P3_split_early.mp4",
-            "P7_split_early.mp4",
-            "P8_split_early.mp4",
-            "P9_split_early.mp4",
-            "P10_split_early.mp4",
-            "P12_split_early.mp4",
-            "P13_split_early.mp4",
-            "P14_split_early.mp4",
-            "P16_split_early.mp4",
-            "P18_split_early.mp4",
-        ]
-    },
+    }
 ];
