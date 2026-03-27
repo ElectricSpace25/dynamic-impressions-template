85dcf382f797e1c10343bd712d4a67c05a9b3837
Yara Studer <yarastuder@gmail.com>
2026-03-27 16:38:26 -0400

    Removed interview decision, professional traits, recruiter instruction framing, and disruption instructions

--- js/content.js
+++ js/content.js
@@ -37,32 +37,30 @@ export const instructionsContent = {
                     type: "html",
                     html: `
                         <p>Thank you for participating in our experiment!</p>
-                        <p>We are researchers interested in how we understand other people in professional settings.</p>
-                        <p>Your job today is simple. <b>There are four parts to our task.</b></p>
+                        <p>We are researchers interested in how we understand other people.</p>
+                        <p>Your job today is simple. <b>There are three parts to our task.</b></p>
                         <div class="callout-box">
                             <ol class="bold-numbers">
-                                <li>View professional video introductions and write down impressions of the speaker.</li>
+                                <li>View videos and type your impressions of the speaker.</li>
                                 <li>Form a final impression of the speaker in the video.</li>
-                                <li>Rate the job candidate on several attributes.</li>
-                                <li>Decide whether to offer the job candidate an interview at the company.</li>
+                                <li>Rate the speaker on several attributes.</li>
                             </ol>
                         </div>
                         <p>You will repeat these steps for each video.</p>
-                        <p>Today, you will take on the role of a professional recruiter. A set of companies (i.e., your clients) have tasked you with reviewing professional video introductions by job candidates. While viewing each video, you should form impressions about the person. Once you have watched the video and formed your impressions, you will make a decision about whether or not the candidate should be offered an interview at the company they applied to.</p>
-                        <p>In the next few panels, you will learn more about each step of the experiment.</p>
+                        <p>In the next few pages, you will learn more about each step of the experiment.</p>
                     `
                 }
             ]
         },
         {
-            title: "Step 1: Watch and describe the job candidate in the video",
+            title: "Step 1: Watch and describe the speaker in the video",
             elements: [
                 {
                     type: "html",
                     html: `
                         <video width="100%" autoplay loop muted src="assets/video/instruct/submitting.mp4"></video>
                         <p>Pause the video whenever you <b>notice a new characteristic</b> about the person or think of a <b>new way to describe them.</b> Pause by clicking anywhere on the video or pressing the spacebar.</p>
-                        <p><b>Enter one word at a time</b>, but you can enter multiple words each time you pause (see video). For example, if you feel like the person is being an annoying student, pause and enter “annoying” and “student” separately. Order does not matter.</p>
+                        <p><b>Enter one word at a time</b>, but you can enter multiple words each time you pause (see video). For example, if you feel like the person is being an annoying student, pause and enter “annoying” and “student” separately. Order does not matter. You can remove words before submitting by clicking the "X" next to the word.</p>
                         <p>Enter whatever comes to mind spontaneously. There are no limits on what you enter! We only ask that you <b>pause and describe the person multiple times.</b></p>
                         <p>Based on past experience, <b>we expect you will pause 2-5 times per video.</b> Please note there is a minimum amount of time that needs to pass between each time you pause the video (2 seconds).</p>
                     `
@@ -76,7 +74,7 @@ export const instructionsContent = {
                     type: "html",
                     html: `
                         <video width="100%" autoplay loop muted src="assets/video/instruct/final.mp4"></video>
-                        <p>Form your final impression of the speaker. <b>Think of this as a list of words you"d use to describe this person to someone else, your summary impression of a person.</b> Once again, enter one word at a time, for as many words as you"d like.</p>
+                        <p>Form your final impression of the speaker. <b>Think of this as a list of words you'd use to describe this person to someone else, your summary impression of a person.</b> Once again, enter one word at a time, for as many words as you"d like.</p>
                     `
                 }
             ]
@@ -88,54 +86,23 @@ export const instructionsContent = {
                     type: "html",
                     html: `
                         <video width="100%" autoplay loop muted src="assets/video/instruct/rating.mp4"></video>
-                        <p>Once you finish watching the video, you will rate the candidate in the video on several attributes using a slider. <b>Please go with your gut feelings, and don't overthink it.</b> You will be evaluating the candidate on these attributes:</p>
+                        <p>Once you finish watching the video, you will rate the speaker in the video on several attributes using sliders. <b>Please go with your gut feelings, and don't overthink it.</b> You will be evaluating the speaker on these attributes:</p>
                         <p><b>Please read the category descriptions carefully below:</b></p>
                         <div class="callout-box">
                             <ul>
                                 <li><b>Openness to new experiences:</b> willingness to try new things and to explore new ideas</li>
-                                <li><b>Conscientiousness:</b> extent to which the candidate seems organized, hardworking, and goal-oriented</li>
-                                <li><b>Extroversion:</b> extent to which the candidate seems energized by social interaction and enjoys being around other people</li>
-                                <li><b>Agreeableness:</b> extent to which the candidate seems cooperative, kind, and trusting</li>
-                                <li><b>Neuroticism:</b> extent to which the candidate seems prone to negative emotions such as anxiety, anger, and sadness</li>
-                                <li><b>Warmth:</b> extent to which the candidate seems friendly, approachable, and likeable</li>
-                                <li><b>Competence:</b> extent to which the candidate seems capable, skilled, and knowledgeable</li>
-                                <li><b>Confidence:</b> extent to which the candidate displays self-assurance, decisiveness, and belief in their own abilities</li>
-                                <li><b>Leadership capacity:</b> extent to which the candidate seems like someone who can guide, inspire, and manage others effectively</li>
-                                <li><b>Ambitiousness:</b> extent to which the candidate displays a strong desire for achievement, advancement, and success</li>
-                                <li><b>Trustworthiness:</b> extent to which the candidate is seems reliable, honest, and dependable in their actions and communications</li>
+                                <li><b>Conscientiousness:</b> extent to which the person seems organized, hardworking, and goal-oriented</li>
+                                <li><b>Extroversion:</b> extent to which the person seems energized by social interaction and enjoys being around other people</li>
+                                <li><b>Agreeableness:</b> extent to which the person seems cooperative, kind, and trusting</li>
+                                <li><b>Neuroticism:</b> extent to which the person seems prone to negative emotions such as anxiety, anger, and sadness</li>
+                                <li><b>Warmth:</b> extent to which the person seems friendly, approachable, and likeable</li>
+                                <li><b>Competence:</b> extent to which the person seems capable, skilled, and knowledgeable</li>
                             </ul>
                         </div>
                     `
                 }
             ]
-        },
-        {
-            title: "Step 4: Make a recruitment decision",
-            elements: [
-                {
-                    type: "html",
-                    html: `
-                        <video width="100%" autoplay loop muted src="assets/video/instruct/decision.mp4"></video>
-                        <p>After evaluating the candidate on all attributes, decide whether to invite them for an interview at the company they applied to.</p>
-                        <p>You will repeat these 4 steps for 10 videos. We encourage you to have fun with this task. Writing more is better than writing less!</p>
-                        <p>After completing all videos, there will be a textbox to provide feedback. We welcome any of your thoughts about ways to improve the task and appreciate your time and effort.</p>
-                    `
-                }
-            ]
-        },
-        {
-            title: "Important Note",
-            elements: [
-                {
-                    type: "html",
-                    html: `
-                        <p>Some of the videos may briefly pause and display a black screen due to technical issues during recording or uploading.</p>
-                        <p>If this occurs, simply wait for the video to resume and continue with the task as usual.</p>
-                        <p>When the video continues, you may keep watching and pausing to describe the person in the video as you have been doing.</p>
-                    `
-                }
-            ]
-        },
+        }
     ]
 };
 
@@ -171,6 +138,8 @@ export const audioCheckContent = {
 };
 
 // --- Rating Impressions ---
+
+// name = name used in data, title = name shown in study
 const traits = [
     { name: "openness", title: "Open to new experiences" },
     { name: "conscientiousness", title: "Conscientious" },
@@ -179,12 +148,9 @@ const traits = [
     { name: "neuroticism", title: "Neurotic" },
     { name: "warmth", title: "Warm" },
     { name: "competence", title: "Competent" },
-    { name: "confidence", title: "Confident" },
-    { name: "leadership", title: "Capable of leadership" },
-    { name: "ambitiousness", title: "Ambitious" },
-    { name: "trustworthiness", title: "Trustworthy" },
 ];
 
+// Dynamically builds sliders based on the above list of traits
 const traitSliders = traits.map(trait => ({
     type: "slider",
     name: trait.name,
@@ -226,36 +192,10 @@ export const ratingContent = {
                     {
                         type: "html",
                         name: "instructions",
-                        html: "<p>Rate the candidate in the video on the following parameters.</p>"
+                        html: "<p>Rate the person in the video on the following parameters.</p>"
                     },
                     ...traitSliders
                 ]
-        },
-        {
-            title: "Recruitment Decision",
-            elements: [
-                {
-                    type: "rating",
-                    name: "interview",
-                    title: "Please decide whether to invite this candidate for an interview.",
-                    autoGenerate: false,
-                    rateCount: 2,
-                    rateValues: [
-                        {
-                            value: "1",
-                            text: "Invite for interview"
-                        },
-                        {
-                            value: "0",
-                            text: "Do not invite for interview"
-                        }
-                    ],
-                    rateMax: 2,
-                    displayMode: "buttons",
-                    isRequired: true
-                }
-            ],
-            showNavigationButtons: false
         }
     ],
 
