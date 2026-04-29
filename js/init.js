7485004ecbe3d4d2c1dd6c90f6edbdb69b783792
Yara Studer <yarastuder@gmail.com>
2026-04-29 15:20:37 -0400

    Save data by name instead of ID, changed from data-ID.csv to ID_data.csv

--- js/init.js
+++ js/init.js
@@ -4,10 +4,10 @@ import { complete } from "./main.js";
 const sessionId = Math.random().toString(36).substring(2) + Date.now().toString(36);
 
 // Function to save data to server
-function saveData(id, data) {
-    var dataToSend = JSON.stringify({ id: id, filedata: data });
-    var success = navigator.sendBeacon("./php/write_data.php", dataToSend);
-    if (config.DEBUG_LOGS) console.log("Data saved to data/" + id + ": " + success);
+function saveData(name, data) {
+    var dataToSend = JSON.stringify({ name: name, filedata: data });
+    var success = navigator.sendBeacon('./php/write_data.php', dataToSend);
+    if (config.DEBUG_LOGS) console.log("Data saved to data/" + name + ": " + success);
 }
 
 // Initialize jsPsych and export it
@@ -15,7 +15,7 @@ export const jsPsych = initJsPsych({
     on_finish: function () {
         if (complete) {
             if (config.DEBUG_SAVE) {
-                jsPsych.data.get().localSave("csv", `data-${sessionId}.csv`);
+                jsPsych.data.get().localSave("csv", `${sessionId}_data.csv`);
             } else {
                 saveData(`data-${sessionId}`, jsPsych.data.get().csv());
                 jsPsych.abortExperiment(config.COMPLETION_MESSAGE);
